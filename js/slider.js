0cea5e8965deb956319af2443013638b88a2a5db
alenglazkov <alen_g@ukr.net>
2023-05-11 01:09:15 +0300

    slider
    slider

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+   $('.slider').slick({
+      rt1: true,
+      dots: true,
+      infinite: true,
+      speed: 1000,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      autoplay: true,
+   });
+});
